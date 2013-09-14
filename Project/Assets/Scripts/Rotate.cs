86376cf05ba9e121f2ae2c128c620921b8ee03e2
Thiago Mezanini Bertoni <thiago@odingamestudio.com.br>
2013-09-13 22:03:50 -0300

    Adiciona projeto padrao

---
+++ Project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	public string nome = "teste";
+
+	public float speed = 5;
+
+    // ############################################################################################ // #UNITY FUNCTION
+    void Start()
+    {
+        //Debug.Log("Teste commit");
+    }
+    // ############################################################################################ // #UNITY FUNCTION
+	void Update () 
+	{
+		transform.Rotate( (Vector3.up * speed) * Time.deltaTime , Space.World );
+	}
+
+	// Teste de comentario 
+}
---
+++ Project/Assets/Scripts/RotateCapsule.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCapsule : MonoBehaviour 
+{
+
+    public float speed = 35;
+
+    // ############################################################################################ // #UNITY FUNCTION
+	void Start ()
+    {
+	    // TEste de fork agora
+	}
+    // ############################################################################################ // #UNITY FUNCTION
+	void Update ()
+    {
+        transform.Rotate((Vector3.right * speed) * Time.deltaTime, Space.World);
+	}
+    // ############################################################################################ // 
+}
